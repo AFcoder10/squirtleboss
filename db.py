817da7391ae9dfd9d03b45b444d0a0e0a56b5764
AFcoder10 <adityafere.6@gmail.com>
2025-12-22 21:11:59 +0530

    db

---
+++ db.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "Pokedia_1166"
+DB_PORT = 5432
+
+try:
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        port=DB_PORT
+    )
+    print("Connected to PostgreSQL!")
+    
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    print("PostgreSQL version:", cur.fetchone())
+    
+    cur.close()
+    conn.close()
+
+except Exception as e:
+    print("Error connecting to PostgreSQL:", e)
